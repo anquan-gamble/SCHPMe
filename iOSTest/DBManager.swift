ee8c883eb05aef64ff3e1e6ce4f3ac01fc2c86d6
Anquan Gamble <34109542+anquan-gamble@users.noreply.github.com>
2020-10-29 15:11:48 -0400

    Add files via upload

--- iOSTest/DBManager.swift
+++ iOSTest/DBManager.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import UIKit
+import SQLite3
+import Toast_Swift
+import FMDB
+
+let fileUrl = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("SCHPMe.sqlite")
+
+class BPData: NSObject {
+    var id: String?
+    var systolic: String?
+    var diastolic: String?
+    var date: String?
+    var time: String?
+}
+class BWData: NSObject {
+    var id: String?
+    var weight: String?
+    var date: String?
+    var time: String?
+}
